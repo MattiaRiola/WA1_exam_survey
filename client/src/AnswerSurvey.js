b105e32eccf0e5868b81bafdd132050056ea94b6
MattiaRiola <s280169@studenti.polito.it>
2021-06-25 13:53:03 +0200

    feat: show questions (and options) of the survey

--- client/src/AnswerSurvey.js
+++ client/src/AnswerSurvey.js
@@ -1,13 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, ListGroup } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
+
 
 
 function AnswerSurvey(props) {
+
+
+
+  const [answers, setAnswers] = useState([]);
+
+
     return (
         <>
             <Col className="bg-light" >
-                <h1>TODO: Insert the mandatory name and use it as use state to force it to be not empty before showing the questions</h1>
+                <h5>TODO: Insert the mandatory name and use it as use state to force it to be not empty before showing the questions</h5>
                 <QuestionTable questions={JSON.parse(props.survey.questions)} />
+                <AnswersTable answers={answers} />
             </Col>
         </>
     );
@@ -16,32 +26,73 @@ function AnswerSurvey(props) {
 function QuestionTable(props) {
     return (
         <>
-            <h3>Questions:</h3>
-            <ListGroup as="ul" variant="flush">
-                {props.questions.map(question =>
-                    <QuestionRow
-                        question={question}
-                    />
-                )
 
-                }
-            </ListGroup>
+            <h1>Questions:</h1>
+
+            {props.questions.map(question =>
+                <Form noValidate>
+
+                    {
+                        (question.options === undefined) ?
+                            <OpenQuestionRow key={question.questionId} question={question} />
+                            : <ClosedQuestionRow question={question} />
+                    }
+                    
+                </Form>
+
+            )
+
+            }
+
         </>
     );
 }
 
-function QuestionRow(props) {
+function OpenQuestionRow(props) {
 
     return (
         <>
-            <ListGroup.Item as="li" variant="secondary">
-                
-                <h5>
-                {props.question.title}
-                </h5>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Label>{props.question.title}</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+            </Form.Group>
 
-            </ListGroup.Item>
+
         </>
+    );
+}
+
+function ClosedQuestionRow(props) {
+
+    return (
+        <>
+
+            <h3>
+                {props.question.title}
+            </h3>
+            {props.question.options.map(option =>
+                <Form.Row>
+
+                <Form.Check inline type="checkbox" id="gridCheck3"
+                />
+                <text>{option.text}</text>
+                </Form.Row>
+
+            )}
+
+        </>
+    );
+}
+
+function AnswersTable(props){
+    return (
+    <>
+    {props.answers.map(a => 
+        <text>
+            question n. {a.questionId}: {a.text === undefined ? a.selectedOptions : a.text}
+        </text>
+        )}
+    </>
     );
 }
 
